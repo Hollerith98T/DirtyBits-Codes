5328e41e0a0931a4f98453cdd374b9507c5d0d99
Giuseppe <43548194+Hollerith98T@users.noreply.github.com>
2022-01-25 16:29:22 +0100

    A template for build everything with the sockets

---
+++ TSDW/Sockets/SocketTemplate/BaseServer.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.net.*;
+
+public class BaseServer
+{
+
+    ServerSocket server = null;
+    Socket socketClient = null;
+
+    int porta = 6789; //porta server
+
+    DataOutputStream out;
+    DataInputStream in;
+
+    public Socket attendi()
+    {
+        try
+        {
+            System.out.println("[0] - Avvio il server..");
+            //inizializziamo il servizio
+            server = new ServerSocket(porta);
+
+            System.out.println("[1] - Server pronto, in ascolto sulla porta:" + porta);
+            // attende la connessione del client
+            socketClient = server.accept();
+
+            //evitiamo connessioni multiple
+            System.out.println("[2] - Connessione completa");
+            server.close();
+            in = new DataInputStream(socketClient.getInputStream());
+            out = new DataOutputStream(socketClient.getOutputStream());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return socketClient;
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        BaseServer s = new BaseServer();
+        s.attendi();
+    }
+}
